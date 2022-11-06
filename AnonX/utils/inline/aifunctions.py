18738ed80ea41569d572abea7bd525f3f45a99d7
shivanshhu <112340896+shivanshhu@users.noreply.github.com>
2022-11-06 11:26:47 +0530

    Update aifunctions.py

--- AnonX/utils/inline/aifunctions.py
+++ AnonX/utils/inline/aifunctions.py
@@ -3,8 +3,8 @@ import requests
 url = "https://iamai.p.rapidapi.com/ask"
 import asyncio
 
-from FallenRobot import telethn
-from FallenRobot.events import register
+from AxonX import telethn
+from AxonX.events import register
 
 
 @register(pattern="Fallen (.*)")
