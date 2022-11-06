6bc4083fb5d7402cfa06c20615557fda4e99369a
shivanshhu <112340896+shivanshhu@users.noreply.github.com>
2022-11-06 11:31:09 +0530

    Update blacklist.py

--- AnonX/utils/inline/blacklist.py
+++ AnonX/utils/inline/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler, run_async
 from telegram.utils.helpers import mention_html
 
-import FallenRobot.modules.sql.blacklist_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_text
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql.approve_sql import is_approved
-from FallenRobot.modules.warns import warn
+import AxonX.modules.sql.blacklist_sql as sql
+from AxonX import LOGGER, dispatcher
+from AxonX.modules.connection import connected
+from AxonX.modules.disable import DisableAbleCommandHandler
+from AxonX.modules.helper_funcs.alternate import send_message, typing_action
+from AxonX.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from AxonX.modules.helper_funcs.extraction import extract_text
+from AxonX.modules.helper_funcs.misc import split_message
+from AxonX.modules.helper_funcs.string_handling import extract_time
+from AxonX.modules.log_channel import loggable
+from AxonX.modules.sql.approve_sql import is_approved
+from AxonX.modules.warns import warn
 
 BLACKLIST_GROUP = 11
 
