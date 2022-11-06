e913635540058026434dda83771928a1f7e8cca0
shivanshhu <112340896+shivanshhu@users.noreply.github.com>
2022-11-06 11:27:14 +0530

    Update alive.py

--- AnonX/utils/inline/alive.py
+++ AnonX/utils/inline/alive.py
@@ -5,9 +5,9 @@ from telegram import __version__ as telever
 from telethon import Button
 from telethon import __version__ as tlhver
 
-from FallenRobot import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
-from FallenRobot import telethn as tbot
-from FallenRobot.events import register
+from AxonX import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
+from AxonX import telethn as tbot
+from AxonX.events import register
 
 PHOTO = [
     "https://telegra.ph/file/6a21ea6677342f43b363e.jpg",
