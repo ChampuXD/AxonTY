3b73a9d52efc78ac8be6c7441fef3081f93a0ac8
shivanshhu <112340896+shivanshhu@users.noreply.github.com>
2022-11-06 11:28:27 +0530

    Update antiflood.py

--- AnonX/utils/inline/antiflood.py
+++ AnonX/utils/inline/antiflood.py
@@ -14,19 +14,19 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-from FallenRobot import TIGERS, WOLVES, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.helper_funcs.chat_status import (
+from AxonX import TIGERS, WOLVES, dispatcher
+from AxonX.modules.connection import connected
+from AxonX.modules.helper_funcs.alternate import send_message
+from AxonX.modules.helper_funcs.chat_status import (
     bot_admin,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql import antiflood_sql as sql
-from FallenRobot.modules.sql.approve_sql import is_approved
+from AxonX.modules.helper_funcs.string_handling import extract_time
+from AxonX.modules.log_channel import loggable
+from AxonX.modules.sql import antiflood_sql as sql
+from AxonX.modules.sql.approve_sql import is_approved
 
 FLOOD_GROUP = 3
 
