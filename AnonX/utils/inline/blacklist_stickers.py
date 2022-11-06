aed5a159ad303e40d5743faf33adde2f88cbe5ab
shivanshhu <112340896+shivanshhu@users.noreply.github.com>
2022-11-06 11:31:44 +0530

    Update blacklist_stickers.py

--- AnonX/utils/inline/blacklist_stickers.py
+++ AnonX/utils/inline/blacklist_stickers.py
@@ -7,16 +7,16 @@ from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandle
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import mention_html, mention_markdown
 
-import FallenRobot.modules.sql.blsticker_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.warns import warn
+import AxonX.modules.sql.blsticker_sql as sql
+from AxonX import LOGGER, dispatcher
+from AxonX.modules.connection import connected
+from AxonX.modules.disable import DisableAbleCommandHandler
+from AxonX.modules.helper_funcs.alternate import send_message
+from AxonX.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from AxonX.modules.helper_funcs.misc import split_message
+from AxonX.modules.helper_funcs.string_handling import extract_time
+from AxonX.modules.log_channel import loggable
+from AxonX.modules.warns import warn
 
 
 @run_async
